33c5e0f9239b0e0e1d3b7c4d33d818431323a181
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-10-31 16:43:20 +0500

    Added: Flexbox и CSS grid homework, Basics JS, ToDo List.

---
+++ js/script.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const generateId = () => `id${Math.round(Math.random() * 1e8).toString(16)}`;
+
+const todoList = document.querySelector('.todo__list'),
+    form = document.querySelector('#form'),
+    noteName = document.querySelector('.note__name');
+
+let dbNote = JSON.parse(localStorage.getItem('calc')) || [];
+
+const renderNote = (note) => {
+    const listItem = document.createElement('li');
+
+    if (note.isDone) {
+        listItem.classList.add('todo__item', 'todo__item-done');
+
+        listItem.innerHTML = `<span class="todo-item-text text-done">${note.description}</span>
+         <div class="buttons">
+            <button class="todo_done visible" data-id="${note.id}">Сделано</button>
+            <button class="todo_delete" data-id="${note.id}">x</button>
+         </div>
+        `;
+    } else {
+        listItem.classList.add('todo__item');
+
+        listItem.innerHTML = `<span class="todo-item-text">${note.description}</span>
+         <div class="buttons">
+            <button class="todo_done" data-id="${note.id}">Сделано</button>
+            <button class="todo_delete" data-id="${note.id}">x</button>
+         </div>
+        `;
+    }
+
+    todoList.append(listItem);
+};
+
+const addNote = (event) => {
+    event.preventDefault();
+    const noteNameValue = noteName.value;
+
+    if (noteNameValue) {
+        const note = {
+            id: generateId(),
+            description: noteNameValue,
+            isDone: false
+        };
+
+        dbNote.push(note);
+        init();
+
+    } else {
+        if (!noteNameValue) { noteName.style.borderColor = 'red' };
+
+        setTimeout(() => {
+            noteName.style.borderColor = ''
+        }, 1000);
+    }
+
+    noteName.value = '';
+};
+
+const deleteNote = (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('todo_delete')) {
+        dbNote = dbNote
+            .filter(note => note.id !== target.dataset.id);
+        console.log(target.dataset.id);
+
+        init();
+    }
+};
+
+const toggleDone = (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('todo_done')) {
+
+        dbNote.forEach(note => {
+            if (note.id == target.dataset.id) {
+
+                if (note.isDone) {
+                    note.isDone = false;
+                } else {
+                    note.isDone = true;
+                }
+
+                init();
+            }
+        });
+    }
+}
+
+const init = () => {
+    todoList.textContent = '';
+    dbNote.forEach(renderNote);
+    localStorage.setItem('calc', JSON.stringify(dbNote));
+};
+
+form.addEventListener('submit', addNote);
+todoList.addEventListener('click', deleteNote);
+todoList.addEventListener('click', toggleDone);
+
+init();
